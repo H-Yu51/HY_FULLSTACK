4b233df0b69d20f0035d8ef11fc5c2443fe7bec6
unknown <2516705605@qq.com>
2020-12-31 19:51:37 +0800

    leetcode

---
+++ leetcode/初级算法leetcode/数组/删除重复项/1.js
@@ -0,0 +1,17 @@
+const removeDuplicates =(nums) => {
+    if(nums.length == 0)
+    {
+        return 0;
+    }
+    let index = 0;
+    for (var i = 0; i < nums.length; i++) {
+        if (nums[i] != nums[i + 1]) {
+            // nums.splice(i, 1);
+            nums[index] = nums[i];
+            index ++;
+        }
+    }
+    return index;
+}
+
+console.log(removeDuplicates([1,1,2])); 
