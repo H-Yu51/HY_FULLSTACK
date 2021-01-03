08536054d754d3bd078d2cd79f3ccb3797e7903b
unknown <2516705605@qq.com>
2021-01-03 17:13:36 +0800

    leetcode

---
+++ leetcode/T239/1.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var maxSlidingWindow = function(nums, k) {
+    const n = nums.length;
+    const prefixMax = new Array(n).fill(0);
+    const suffixMax = new Array(n).fill(0);
+    for (let i = 0; i < n; i++) {
+        if (i % k === 0) {
+            prefixMax[i] = nums[i];
+        } else {
+            prefixMax[i] = Math.max(prefixMax[i - 1], nums[i]);
+        }
+    }
+    for (let i = n - 1; i >= 0; --i) {
+        if (i === n || (i + 1) % k === 0) {
+            suffixMax[i] = nums[i];
+        } else {
+            suffixMax[i] = Math.max(suffixMax[i + 1], nums[i]);
+        }
+    }
+    const ans = [];
+    for (let i = 0; i < n - k + 1; i++) {
+        ans.push(Math.max(suffixMax[i], prefixMax[i + k - 1]));
+    }
+    return ans;
+
+}
