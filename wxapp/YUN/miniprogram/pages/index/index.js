d75b54c989c793334e46d38a1d725bc7f843a439
unknown <2516705605@qq.com>
2021-02-06 22:21:04 +0800

    wxapp

--- wxapp/YUN/miniprogram/pages/index/index.js
+++ wxapp/YUN/miniprogram/pages/index/index.js
@@ -1,3 +1,70 @@
+// miniprogram/pages/index/index.js
 Page({
-  
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imgUrls:[
+      'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/01d048d6ca5fea696b60c0431e24fbf3.jpg?thumb=1&w=1533&h=575&f=webp&q=90',
+      'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/c7f07012cae3bacba372aeca282d6a65.jpg?w=2452&h=920',
+      'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/0ef4160c861b998239bce9adb82341e7.jpg?thumb=1&w=1533&h=575&f=webp&q=90'
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
 })
