07ee067d13f35281b82253c6906586331eda7140
unknown <2516705605@qq.com>
2021-02-17 22:24:10 +0800

    leetcode

---
+++ leetcode/T566/1.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[][]} nums
+ * @param {number} r
+ * @param {number} c
+ * @return {number[][]}
+ */
+var matrixReshape = function(nums, r, c) {
+    const m = nums.length;
+        const n = nums[0].length;
+        if (m * n != r * c) {
+            return nums;
+        }
+    
+        const ans = new Array(r).fill(0).map(() => new Array(c).fill(0));
+        for (let x = 0; x < m * n; ++x) {
+            ans[Math.floor(x / c)][x % c] = nums[Math.floor(x / n)][x % n];
+        }
+        return ans;
+    };
