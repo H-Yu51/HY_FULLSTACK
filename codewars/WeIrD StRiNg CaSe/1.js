a3cf415b97edda5330f00b230cd7e4f6a14ab6a6
unknown <2516705605@qq.com>
2021-03-09 16:12:53 +0800

    codewars

--- codewars/WeIrD StRiNg CaSe/1.js
+++ codewars/WeIrD StRiNg CaSe/1.js
@@ -3,6 +3,6 @@ const toWeirdCase =(string) => {
 }
 
 
-console.log(toWeirdCase("This is a test"));
-console.log(toWeirdCase("Weird string case" ));
+console.log(toWeirdCase("This is a test"));//'ThIs Is A TeSt'
+console.log(toWeirdCase("Weird string case" )); //"WeIrD StRiNg CaSe"
 console.log(toWeirdCase("String"));
