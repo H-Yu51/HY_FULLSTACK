f720fa2da5c1be157f011b2536217528cf818837
unknown <2516705605@qq.com>
2021-03-16 08:18:30 +0800

    leetcode

--- leetcode/初级算法leetcode/数组/存在重复元素/1.js
+++ leetcode/初级算法leetcode/数组/存在重复元素/1.js
@@ -1,14 +1,17 @@
-/**
- * @param {number[]} nums
- * @return {boolean}
- */
-var containsDuplicate = function(nums) {
-    nums.sort((a,b) => a-b);
-    console.log(nums);
 
+var containsDuplicate = function(nums) {
+    nums.sort((a, b) => a - b);
+    console.log(nums);
+    const n = nums.length;
+    for (let i = 0; i < n - 1; i++) {
+        if (nums[i] === nums[i + 1]) {
+            return true;
+        }
+    }
+    return false;
 };
 
 
-console.log([1,2,3,1]);
-console.log([1,2,3,4]);
-console.log( [1,1,1,3,3,4,3,2,4,2]);
+console.log(containsDuplicate([1,2,3,1]))
+console.log(containsDuplicate([1,2,3,4]))
+console.log(containsDuplicate([1,1,1,3,3,4,3,2,4,2]))
