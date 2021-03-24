7ba34dbc27c1573c81d6c09e0e322b58033f064b
unknown <2516705605@qq.com>
2021-03-24 10:40:30 +0800

    js

---
+++ js/hy/10.js
@@ -0,0 +1,8 @@
+console.log(null >= 0);
+console.log(Number(null));
+console.log(null == 0);
+console.log([] == ![]);
+console.log(false == 0);
+
+console.log(null == undefined);
+console.log([]+{}  );
