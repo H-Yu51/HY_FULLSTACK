1ab10f0a734fba1b78a7c56be1812d7814c1799c
unknown <2516705605@qq.com>
2021-03-25 18:32:08 +0800

    react

---
+++ react/umi-demo/src/pages/about.tsx
@@ -0,0 +1,16 @@
+import './about.less'
+import {Link} from 'umi'
+
+
+
+export default function AboutPage(){
+
+    return(
+        <div>
+            <Link to ="/">Index</Link>
+            <div>pageAbout</div>
+        </div>
+        
+    )
+
+}
