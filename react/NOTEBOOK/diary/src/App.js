599775bb7ba87f36defc0b9023e2a4bbc8be4e02
unknown <2516705605@qq.com>
2021-04-14 16:33:33 +0800

    first commit

---
+++ react/NOTEBOOK/diary/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import {Button } from 'antd-mobile'
+
+function App() {
+  return (
+    <div className="App">
+      <Button type="primary">测试</Button>
+    </div>
+  );
+}
+
+export default App;
