45ae101d0ceadf6447f739bea3e154305b1073dd
unknown <2516705605@qq.com>
2021-04-17 19:45:01 +0800

    Promise

---
+++ interview/PromiseInterview/4.js
@@ -0,0 +1,69 @@
+class Promise{
+  constructor(executor){
+    this.state = 'pending';
+    this.value = undefined;
+    this.reason = undefined;
+    // 成功存放的数组
+    this.onResolvedCallbacks = [];
+    // 失败存放法数组
+    this.onRejectedCallbacks = [];
+    let resolve = value => {
+      if (this.state === 'pending') {
+        this.state = 'fulfilled';
+        this.value = value;
+        // 一旦resolve执行，调用成功数组的函数
+        this.onResolvedCallbacks.forEach(fn=>fn());
+      }
+    };
+    let reject = reason => {
+      if (this.state === 'pending') {
+        this.state = 'rejected';
+        this.reason = reason;
+        // 一旦reject执行，调用失败数组的函数
+        this.onRejectedCallbacks.forEach(fn=>fn());
+      }
+    };
+    try{
+      executor(resolve, reject);
+    } catch (err) {
+      reject(err);
+    }
+  }
+  then(onFulfilled,onRejected) {
+    if (this.state === 'fulfilled') {
+      onFulfilled(this.value);
+    };
+    if (this.state === 'rejected') {
+      onRejected(this.reason);
+    };
+    // 当状态state为pending时
+    if (this.state === 'pending') {
+      // onFulfilled传入到成功数组
+      this.onResolvedCallbacks.push(()=>{
+        onFulfilled(this.value);
+      })
+      // onRejected传入到失败数组
+      this.onRejectedCallbacks.push(()=>{
+        onRejected(this.reason);
+      })
+    }
+  }
+}
+let p1 = new Promise((resolve, reject) => {
+
+  console.log(0);
+  setTimeout(() => {   
+      // reject('我不爱你了')
+      console.log('setTimeout');   
+      resolve(10)
+      // 可能有错误
+      // throw new Error('是你的错')      
+  }, 1000)
+}).then((data) => {
+  console.log(data, '++++++++++');
+})
+
+// 0
+// 一秒
+// 10 ++++++++++
+// setTimeout
